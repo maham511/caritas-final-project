f59f0705d7bd6fe358e410142bf4f4868bb41d18
mohammed <mohammedalabdulhamid@gmail.com>
2022-01-17 20:14:57 +0000

    services & aboutUs component

---
+++ src/components/aboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const aboutUs = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit dicta quaerat harum totam tempore natus numquam eum in repellendus!</p>
+        </div>
+    )
+}
---
+++ src/components/services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const services = () => {
+    return (
+        <div>
+           <ul>
+               <li>servece1</li>
+               <li>service2</li>
+               <li>service3</li>
+               <li>service4</li>
+               </ul> 
+        </div>
+    )
+}
