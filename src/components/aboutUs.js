1298c8f6dce621ff40f9bd9ba4090412601c3456
Texx45 <tcalderbank@hotmail.com>
2022-01-22 14:24:18 +0000

    completed aboutUs.js section

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 const AboutUs = () => (
-  <div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit dicta
-      quaerat harum totam tempore natus numquam eum in repellendus!
-    </p>
+  <div className="m-2 bg-red-600 font-lato w-full p-4">
+    <div className="bg-red">
+      <h2 className="text-center text-white text-4xl mt-6 w-full bg-red">
+        Caritas Refugee Education
+      </h2>
+      <p className="text-black p-4 bg-white m-6 font-bold">
+        ”I went from being destitute and living in emergency accommodation, to
+        getting a qualification with Caritas and now succeeding in my university
+        course.” Musaab – former student
+      </p>
+      <p className="text-white p-4">
+        Caritas Refugee Education works alongside refugees and asylum seekers in
+        order to assist in their journey of wellbeing and integration. We do
+        this by providing high quality language lessons, delivered in a
+        welcoming community environment, mentoring into further education and
+        employment, and wrap-around well-being services.
+      </p>
+    </div>
   </div>
 )
 
