d2ee15960b5cdd19751f429c6aa3c412bef811fd
Texx45 <tcalderbank@hotmail.com>
2022-01-22 11:47:23 +0000

    completed services.js

--- src/components/services.js
+++ src/components/services.js
@@ -8,16 +8,16 @@ const Services = () => (
       </h2>
     </div>
     <div className="w-full">
-      <ul className="grid grid-cols-1 sm:grid-cols-2 ">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 list-disc">
         <li className="border-2 m-6 p-4 col-span-1 text-base">
-          <span className="font-bold text-large pr-1">• Education:</span>English
+          <span className="font-bold text-large pr-1">Education:</span>English
           language is the main barrier to young refugees entering training,
           further education and employment. We offer over 60 hours of English
           tuition each week, taught by fully qualified teachers, at seven
           different language levels.
         </li>
         <li className="border-2 m-6 p-4 col-span-1 text-base">
-          <span className="font-bold text-large pr-1">• Higher Education:</span>
+          <span className="font-bold text-large pr-1">Higher Education:</span>
           Employability and training, we undertake detailed case-work with
           refugees, helping them to find and apply for courses, as well as to
           skill-up to the required level for given courses. Information, advice
@@ -25,7 +25,7 @@ const Services = () => (
           information needed to enter the jobs market.
         </li>
         <li className="border-2 m-6 p-4 col-span-1 text-base">
-          <span className="font-bold text-large pr-1">• Qualifications:</span>
+          <span className="font-bold text-large pr-1">Qualifications:</span>
           We offer bespoke training for vital qualifications (IELTS, Functional
           Skills English, Functional Skills Maths) needed to enter training,
           employment and further education. We also offer pathways to funding
@@ -33,7 +33,7 @@ const Services = () => (
         </li>
         <li className="border-2 m-6 p-4 col-span-1 text-base">
           <span span className="font-bold text-large pr-1">
-            • Well-being support:
+            Well-being support:
           </span>
           We offer referrals to other Caritas services and external
           organisations on a range of welfare issues, including housing,
