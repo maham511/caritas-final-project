6faa9e8e0c8beebb840307bc88fbf18e2d79f9e6
maham511 <maha.malik511@gmail.com>
2022-01-26 12:47:00 +0000

    add-h2-to-video

--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -4,6 +4,9 @@ const VideoSection = () => {
   return (
     <div className="flex items-center justify center m-2">
       <div className="flex-1 max-w-3xl m-auto">
+        <h2 className="text-center text-4xl mt-6 p-4 w-full">
+          See Our Work in Action
+        </h2>
         <div className="aspect-w-16 aspect-h-9">
           <iframe
             className="m-auto p-4"
