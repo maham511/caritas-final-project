d984fac15ce2d8bab8817ab8033e91b471bff89a
Texx45 <tcalderbank@hotmail.com>
2022-01-26 16:31:52 +0000

    Completed footer component

--- src/components/Footer/privacy.js
+++ src/components/Footer/privacy.js
@@ -2,8 +2,22 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Privacy = () => (
-  <div>
-    <Link to="/">privacy policy here</Link>
+  <div className="p-4 text-center">
+    <p>
+      Caritas Diocese of Salford &copy; 2022 <span className='pl-1' >|</span> 
+      <Link
+        className="pl-2"
+        to="http://www.caritassalford.org.uk/cookie-policy/"
+      >
+        Cookie Policy
+      </Link>
+      <Link
+        className="pl-2"
+        to="http://www.caritassalford.org.uk/privacy-policy/"
+      >
+        Privacy Policy
+      </Link>
+    </p>
   </div>
 )
 
