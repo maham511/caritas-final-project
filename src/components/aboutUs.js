31ecab4e050f7d2d4edbfa2bef2ca55ab5a43aa8
maham511 <maha.malik511@gmail.com>
2022-01-28 00:15:00 +0000

    added text color to global css, removed from individual components

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const AboutUs = () => (
-  <div className="m-2 bg-red-600 p-4 text-white">
+  <div className="m-2 bg-red-600 p-4">
     <div className="bg-red">
-      <h2 className="text-center text-4xl mt-6 bg-red">
+      <h2 className="text-center text-4xl mt-6 bg-red text-white">
         Caritas Refugee Education
       </h2>
-      <p className=" p-4 bg-white m-6 font-bold text-gray-600">
+      <p className="p-4 bg-white m-6 font-bold">
         ”I went from being destitute and living in emergency accommodation, to
         getting a qualification with Caritas and now succeeding in my university
         course.” Musaab – former student
       </p>
-      <p className="p-4">
+      <p className="p-4 text-white">
         Caritas Refugee Education works alongside refugees and asylum seekers in
         order to assist in their journey of wellbeing and integration. We do
         this by providing high quality language lessons, delivered in a
--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -4,7 +4,7 @@ const VideoSection = () => {
   return (
     <div className="flex m-2">
       <div className="flex-1 max-w-4xl m-auto">
-        <h2 className="text-center text-xl mt-6 p-4 w-full md:text-4xl text-gray-600">
+        <h2 className="text-center text-xl mt-6 p-4 w-full md:text-4xl">
           See Our Work in Action
         </h2>
         <div className="aspect-w-16 aspect-h-9">
