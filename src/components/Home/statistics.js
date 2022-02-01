ac7ce333693abe1748746689257a9584f6c37147
maham511 <maha.malik511@gmail.com>
2022-02-01 18:49:21 +0000

    Further css changes to Stats section

--- src/components/Home/statistics.js
+++ src/components/Home/statistics.js
@@ -1,49 +1,31 @@
 import React from 'react'
-import statsImage from '../../assets/Stats.png'
 
 const Statistics = () => {
   return (
     <div className="flex m-2 bg-gray-200">
-      <div className="flex-1 max-w-4xl m-auto">
+      <div className="flex-1 max-w-4xl m-auto mb-6">
         <h2 className="text-center mt-6 p-4 w-full">Our Impact on Students</h2>
-        <div className="flex flex-col md:flex-row justify-around">
-          <div className="bg-white rounded m-2 p-4">
-            <div className="text-4xl font-extrabold">
-              <p>87%</p>
-            </div>
-            <div>
-              <p>
-                Students agreed English classes helped them feel less isolated
-              </p>
-            </div>
+        <div className="grid grid-cols-1 p-2 md:gap-4 md:grid-cols-3 rounded md:text-center">
+          <div className="bg-white m-2 p-4">
+            <p className="text-4xl font-extrabold pb-2">87%</p>
+            <p>
+              Students agreed English classes helped them feel less isolated
+            </p>
           </div>
-          <div className="bg-white rounded m-2 p-4">
-            <div className="text-4xl font-extrabold">
-              <p>85%</p>
-            </div>
-            <div>
-              <p>
-                Students agreed the program contributed positively to their
-                happiness and well-being
-              </p>
-            </div>
+          <div className="bg-white m-2 p-4">
+            <p className="text-4xl font-extrabold pb-2">85%</p>
+            <p>
+              Students agreed the program contributed positively to their
+              happiness and well-being
+            </p>
           </div>
-          <div className="bg-white rounded m-2 p-4">
-            <div className="text-4xl font-extrabold">
-              <p>90%</p>
-            </div>
-            <div>
-              <p>
-                Students that used the education service alongside other Caritas
-                services
-              </p>
-            </div>
+          <div className="bg-white m-2 p-4">
+            <p className="text-4xl font-extrabold pb-2">90%</p>
+            <p>
+              Students used the education service alongside other Caritas
+              services
+            </p>
           </div>
-          {/* <img
-            className="m-auto p-4"
-            src={statsImage}
-            alt="Statistics for student results"
-          /> */}
         </div>
       </div>
     </div>
