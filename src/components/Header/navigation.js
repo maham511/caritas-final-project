123bc2a810ae0e9c65c0d0a5f3f6c6475bebfccc
maham511 <maha.malik511@gmail.com>
2022-02-01 21:07:02 +0000

    Reverted accidentally commited changes to Navbar

--- src/components/Header/navigation.js
+++ src/components/Header/navigation.js
@@ -3,8 +3,8 @@ import { Link } from 'gatsby'
 
 const Navigation = () => (
   <nav role="navigation" aria-label="Main">
-    <ul className="flex text-2xl text-gray-600 uppercase border-solid border-red-600">
-      <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:border-solid hover:border-red-600 transition ease-out duration-500">
+    <ul className="flex text-2xl text-gray-600 uppercase">
+      <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:text-white hover:bg-red-600 transition ease-out duration-500">
         <Link to="/">Home</Link>
       </li>
       <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:text-white hover:bg-red-600 transition ease-out duration-500">
