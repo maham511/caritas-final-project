ffe75fd7c3203266c202f88ec055717bbf0f8950
maham511 <maha.malik511@gmail.com>
2022-02-01 20:55:16 +0000

    Added red bg to Stats cards, removed margin top from button

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Button = ({ text, icon, onClick, className, type }) => {
   return (
-    <div data-testid="main-button" className="flex justify-center mt-6">
+    <div data-testid="main-button" className="flex justify-center">
       <button
         className={className}
         onClick={(event) => onClick(event)}
--- src/components/Home/statistics.js
+++ src/components/Home/statistics.js
@@ -5,22 +5,22 @@ const Statistics = () => {
     <div className="flex m-2 bg-gray-200">
       <div className="flex-1 max-w-4xl m-auto mb-6">
         <h2 className="text-center mt-6 p-4 w-full">Our Impact on Students</h2>
-        <div className="grid grid-cols-1 p-2 md:gap-4 md:grid-cols-3 rounded md:text-center">
-          <div className="bg-white m-2 p-4">
-            <p className="text-4xl font-extrabold pb-2">87%</p>
+        <div className="grid grid-cols-1 p-2 md:gap-4 md:grid-cols-3 md:text-center">
+          <div className="m-2 p-4 bg-red-600 text-white rounded">
+            <p className="text-2xl md:text-4xl font-extrabold pb-2">87%</p>
             <p>
               Students agreed English classes helped them feel less isolated
             </p>
           </div>
-          <div className="bg-white m-2 p-4">
-            <p className="text-4xl font-extrabold pb-2">85%</p>
+          <div className="m-2 p-4 bg-red-600 text-white rounded">
+            <p className="text-2xl md:text-4xl font-extrabold pb-2">85%</p>
             <p>
               Students agreed the program contributed positively to their
               happiness and well-being
             </p>
           </div>
-          <div className="bg-white m-2 p-4">
-            <p className="text-4xl font-extrabold pb-2">90%</p>
+          <div className="m-2 p-4 bg-red-600 text-white rounded">
+            <p className="text-2xl md:text-4xl font-extrabold pb-2">90%</p>
             <p>
               Students used the education service alongside other Caritas
               services
