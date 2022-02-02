b5fad5b63fa3c375d00dbba7651751606cdbb7fa
maham511 <maha.malik511@gmail.com>
2022-02-02 17:35:32 +0000

    Header and Navbar styles updated

--- src/components/Header/navigation.js
+++ src/components/Header/navigation.js
@@ -3,14 +3,11 @@ import { Link } from 'gatsby'
 
 const Navigation = () => (
   <nav role="navigation" aria-label="Main">
-    <ul className="flex text-2xl text-gray-600 uppercase">
-      <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:text-white hover:bg-red-600 transition ease-out duration-500">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:text-white hover:bg-red-600 transition ease-out duration-500">
+    <ul className="flex text-base text-gray-600 uppercase">
+      <li className="md:text-xl font-bold px-2 md:px-4 py-2 m-1 border-t-8 border-transparent hover:border-red-600 transition ease-out duration-500">
         <Link to="/studentWall/">Student Wall</Link>
       </li>
-      <li className="text-base md:text-2xl px-4 py-2 m-1 rounded hover:text-white hover:bg-red-600 transition ease-out duration-500">
+      <li className="md:text-xl font-bold px-2 md:px-4 py-2 m-1 border-t-8 border-transparent hover:border-red-600 transition ease-out duration-500">
         <Link to="/caseStudies/">Case Studies</Link>
       </li>
     </ul>
