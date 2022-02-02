e05c516b996c3b1d85c14f58b6b76cd0659e4493
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-02-02 12:36:01 +0000

    fixed long text contentful

--- src/templates/singleCaseStudy.js
+++ src/templates/singleCaseStudy.js
@@ -18,7 +18,7 @@ class CaseStudyTemplate extends React.Component {
     }
 
     const caseStudy = get(this.props, 'data.contentfulCaseStudies')
-
+    console.log(caseStudy)
     return (
       <Layout location={this.props.location}>
         <div className="grid grid-cols-1 p-2 m-2 md:grid-cols-2 p-6 bg-gray-200">
@@ -32,7 +32,14 @@ class CaseStudyTemplate extends React.Component {
             <h2 className="text-xl md:text-3xl">{caseStudy.title}</h2>
           </div>
           <div className="order-4 col-span-1 md:col-span-2 grow text-justify p-2 pt-6">
-            <p>{caseStudy.story.internal.content}</p>
+            {/* <p>{caseStudy.story.internal.content}</p> */}
+            <div
+              dangerouslySetInnerHTML={{
+                __html:
+                  caseStudy.childContentfulCaseStudiesStoryTextNode
+                    .childMarkdownRemark.html,
+              }}
+            />
           </div>
         </div>
         <Button
@@ -71,9 +78,9 @@ export const pageQuery = graphql`
           height: 212
         )
       }
-      story {
-        internal {
-          content
+      childContentfulCaseStudiesStoryTextNode {
+        childMarkdownRemark {
+          html
         }
       }
     }
